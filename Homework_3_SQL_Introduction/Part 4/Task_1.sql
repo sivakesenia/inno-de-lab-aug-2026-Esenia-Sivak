1034539cae386a230e827b91760f40664ea45061
Сивак Есения <sivakesenia@gmail.com>
2026-08-13 03:05:48 +0300

    Add homework 3 Part 4

--- Homework_3_SQL_Introduction/Part 4/Task_1.sql
+++ Homework_3_SQL_Introduction/Part 4/Task_1.sql
@@ -1,11 +1,8 @@
 -- select the list of customers selected by descending order
 SELECT
-	order_id,
-	customer_id,
-	item,
-	amount,
-	SUM(amount) OVER() AS total_by_customer
+    c.first_name,
+    c.age
 FROM
-	orders o
-JOIN customers c ON
-	o.customer_id = c.customer_id
+    customers c
+ORDER BY
+    c.age DESC
